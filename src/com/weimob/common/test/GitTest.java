b26848a4c39f3003a63bf19b794bd9df56346a22
jww <729416812@qq.com>
2018-11-09 16:43:12 +0800

    git test

--- src/com/weimob/common/test/GitTest.java
+++ src/com/weimob/common/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is develop");
         System.out.println("第二次修改");
+        System.out.println("develop 第三次修改");
     }
 }
