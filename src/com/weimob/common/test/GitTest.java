76492be1719b15fbd1fe1688c02e6075aaffda03
jww <729416812@qq.com>
2018-11-09 16:35:53 +0800

    git test

---
+++ src/com/weimob/common/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.weimob.common.test;
+
+/**
+ * Created by Curry on 2018/11/9
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is develop");
+    }
+}
