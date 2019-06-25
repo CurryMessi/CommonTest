13cbefbd076cdc987a2d6bb36073446e743446b2
jww <729416812@qq.com>
2019-06-25 17:24:35 +0800

    develop 第十二次修改

--- src/com/weimob/common/test/GitTest.java
+++ src/com/weimob/common/test/GitTest.java
@@ -25,5 +25,6 @@ public class GitTest {
         System.out.println("develop 第十一次修改");
         System.out.println("master  第十二次修改");
         System.out.println("master  第十三次修改");
+        System.out.println("develop 第十二次修改");
     }
 }
