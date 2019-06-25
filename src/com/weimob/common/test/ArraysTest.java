f48d280c13b5eef11c5bbc8f5c162b66fb9d3dfe
jww <729416812@qq.com>
2019-06-25 14:32:08 +0800

    master 第八次修改

---
+++ src/com/weimob/common/test/ArraysTest.java
@@ -0,0 +1,23 @@
+package com.weimob.common.test;
+
+import java.util.Arrays;
+
+/**
+ * @author Curry
+ * @date 2019/4/20
+ */
+public class ArraysTest {
+    //
+    private static boolean flag;
+
+    public static void main(String[] args) {
+//        int[] A = {6,7,8,2,1,3,4,1};
+//        int maxVal = Arrays.stream(A).max().getAsInt();
+//        System.out.println(maxVal);
+//        System.out.println(flag);
+           String name=null;
+           String name1=(name==null?"123":("123"+"("+name+")"));
+            System.out.println(name1);
+
+    }
+}
