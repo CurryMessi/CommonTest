921b25525ff07d97d99eee40e4fcb115cc3c4234
jww <729416812@qq.com>
2019-06-25 13:07:16 +0800

    Revert "update"
    This reverts commit d104fd6

--- src/com/weimob/common/test/GitTest.java
+++ src/com/weimob/common/test/GitTest.java
@@ -12,6 +12,5 @@ public class GitTest {
         System.out.println("master 第四次修改");
         System.out.println("develop 第四次修改");
         System.out.println("master 第五次修改");
-        System.out.println("master 第六次修改");
     }
 }
